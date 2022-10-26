5817e01869225baf5415a2f223a988fc75b0051b
William Candillon <wcandillon@gmail.com>
2022-10-26 11:04:18 +0200

    Add first test

--- package/src/skia/__tests__/Paragraph.spec.ts
+++ package/src/skia/__tests__/Paragraph.spec.ts
@@ -4,31 +4,37 @@ import { processResult } from "../../__tests__/setup";
 
 import { setupSkia } from "./setup";
 
+const roboto = resolveFile("skia/__tests__/assets/Roboto-Regular.ttf");
+const noto = resolveFile("skia/__tests__/assets/NotoColorEmoji.ttf");
+
 describe("Paragraph", () => {
-  it("should display the paragraph layout properly", async () => {
-    const { surface, canvas: rnCanvas } = setupSkia();
+  it("should display the paragraph layout properly using the typeface provider", async () => {
+    const { surface, canvas: rnCanvas, width } = setupSkia();
     const canvas = rnCanvas as JsiSkCanvas;
-    const roboto = resolveFile("skia/__tests__/assets/Roboto-Regular.ttf");
-    const noto = resolveFile("skia/__tests__/assets/NotoColorEmoji.ttf");
-    const fontMgr = CanvasKit.FontMgr.FromData(roboto, noto)!;
-    expect(fontMgr).not.toBeNull();
-    expect(fontMgr.countFamilies()).toBe(2);
+    const fontSrc = CanvasKit.TypefaceFontProvider.Make();
+    fontSrc.registerFont(roboto, "Roboto");
+    fontSrc.registerFont(noto, "Noto Color Emoji");
     const paraStyle = new CanvasKit.ParagraphStyle({
       textStyle: {
         color: CanvasKit.BLACK,
         fontFamilies: ["Roboto", "Noto Color Emoji"],
-        fontSize: 50,
+        fontSize: 35,
       },
       textAlign: CanvasKit.TextAlign.Left,
-      maxLines: 7,
+      maxLines: 4,
       ellipsis: "...",
     });
-    const str = "The quick brown fox";
-    const builder = CanvasKit.ParagraphBuilder.Make(paraStyle, fontMgr);
+    const str =
+      "The quick brown fox 🦊 ate a zesty hamburgerfons 🍔.\nThe 👩‍👩‍👧‍👧 laughed.";
+    const builder = CanvasKit.ParagraphBuilder.MakeFromFontProvider(
+      paraStyle,
+      fontSrc
+    );
     builder.addText(str);
     const paragraph = builder.build();
+    paragraph.layout(width);
     expect(paragraph.getHeight()).not.toBe(0);
     canvas.ref.drawParagraph(paragraph, 0, 0);
-    processResult(surface, "snapshots/drawings/paragraph.png", true);
+    processResult(surface, "snapshots/drawings/paragraph.png");
   });
 });
