143f74381ab9a374cbb584e19c2ab490c13f7eef
William Candillon <wcandillon@gmail.com>
2023-02-07 19:06:54 +0100

    :wrench:

--- example/src/Examples/Globe/Globe.tsx
+++ example/src/Examples/Globe/Globe.tsx
@@ -15,6 +15,7 @@ import {
 import React from "react";
 import { useWindowDimensions } from "react-native";
 
+// https://www.shadertoy.com/view/Ml2XRV
 const source = Skia.RuntimeEffect.Make(`
 uniform shader image;
 uniform float iTime;
@@ -28,127 +29,56 @@ const int MAX_STEPS = 100;
 const float MAX_DIST  = 100;
 const float SURF_DIST = .001;
 
-mat2 Rot(float a) {
-    float s=sin(a), c=cos(a);
-    return mat2(c, -s, s, c);
-}
 
-float Hash21(vec2 p) {
-    p = fract(p*vec2(123.34,233.53));
-    p += dot(p, p+23.234);
-    return fract(p.x*p.y);
-}
-
-float sdBox(vec3 p, vec3 s) {
-    p = abs(p)-s;
-	return length(max(p, 0.))+min(max(p.x, max(p.y, p.z)), 0.);
-}
-
-float GetDist(vec3 p) {
-    float d = sdBox(p, vec3(1));
+vec2 rotate13(
     
-    d = length(p)-1.5;
-    
-    vec2 uv = vec2(atan(p.x, p.z)/6.2832, p.y/3.)+.5;
-    float disp = image.eval(uv).r;
-    
-    disp *= smoothstep(1.4, 1., abs(p.y));
-    d -= disp*.3;
-    
-    return d*.7;
+  vec2  lrf, 
+  float guvf) {
+float vf  = cos(guvf); 
+  float n   = sin(guvf);
+  mat2  wbxr = mat2(vf,n,n,-vf);
+  
+  return lrf * wbxr;
 }
-
-vec3 Transform(vec3 p) {
-     p.xy *= Rot(iTime*.4);
-     p.xz *= Rot(iTime*.2);
-     
-     return p;
-}
-
-float RayMarch(vec3 ro, vec3 rd) {
-	float dO=0.;
-    
-    for(int i=0; i<MAX_STEPS; i++) {
-    	vec3 p = Transform(ro + rd*dO);
-       
-        float dS = GetDist(p);
-        dO += dS;
-        if(dO>MAX_DIST || abs(dS)<SURF_DIST) break;
-    }
-    
-    return dO;
-}
-
-vec3 GetNormal(vec3 p) {
-	float d = GetDist(p);
-    vec2 e = vec2(.001, 0);
-    
-    vec3 n = d - vec3(
-        GetDist(p-e.xyy),
-        GetDist(p-e.yxy),
-        GetDist(p-e.yyx));
-    
-    return normalize(n);
-}
-
-vec3 GetRayDir(vec2 uv, vec3 p, vec3 l, float z) {
-    vec3 f = normalize(l-p),
-        r = normalize(cross(vec3(0,1,0), f)),
-        u = cross(f,r),
-        c = f*z,
-        i = c + uv.x*r + uv.y*u,
-        d = normalize(i);
-    return d;
-}
-
-
 
 half4 main( vec2 fragCoord )
 {
-    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
-	vec2 m = iMouse.xy/iResolution.xy;
-    
-    vec3 col = vec3(0);
-    
-    vec3 ro = vec3(0, 3, -3);
-    ro.yz *= Rot(-m.y*3.14+1.);
-    ro.xz *= Rot(-m.x*6.2831);
-    
-    vec3 rd = GetRayDir(uv, ro, vec3(0), 1.);
-
-    float d = RayMarch(ro, rd);
-    
-    if(d<MAX_DIST) {
-    	vec3 p = Transform(ro + rd * d);
-        
-        
-    	vec3 n = GetNormal(p);
-        
-    	float dif = dot(n, normalize(vec3(1,2,3)))*.5+.5;
-    	col += dif*dif;
-        
-        vec3 colXZ = image.eval(p.xz*.5+.5).rgb;
-        vec3 colYZ = image.eval(p.yz*.5+.5).rgb;
-        vec3 colXY = image.eval(p.xy*.5+.5).rgb;
-        
-        n = abs(n);
-        
-        n *= pow(n, vec3(2));
-        n /= n.x+n.y+n.z;
-        
-        col = colYZ*n.x + colXZ*n.y + colXY*n.z;
-        
-        uv = vec2(atan(p.x, p.z)/6.2832, 2.*p.y/3.)+.5;
-        
-        uv.x = fract(uv.x-iTime*.1);
-        col *= dif;
-    }
-    
+vec2 uv = fragCoord.xy / iResolution.xy;
+  uv -= 0.5;
+  uv.x *= iResolution.x / iResolution.y;
+  uv *= 2.2;
+  vec2 m = (iMouse.xy / iResolution.xy) - 0.5;
+  bool clicked = false;
+  if (length(uv) > 1.0) {
+    return vec4(0.0,0.0,0.0,1.0);
+  }
+  uv = mix(uv,normalize(uv)*(2.0*asin(length(uv)) / 3.1415926),0.5);
+  vec3 n = vec3(uv, sqrt(1.0 - uv.x*uv.x - uv.y*uv.y));
+  uv = normalize(uv)*(2.0*asin(length(uv)) / 3.1415926);
   
-    if (col.rgb == vec3(0, 0, 0)) {
-      return vec4(col,0.0);
-    }
-    return vec4(col,1.0);
+  float phase = iTime * 0.33;
+  float phase2 = -m.y * 4.0;
+  
+  vec3 lightvec = vec3(1.0,1.0,0.0);
+  if (clicked) {
+    lightvec.yz = rotate13(lightvec.yz, phase2 * 0.5 * 3.1415926);
+  uv.y += phase2;
+  } else {
+    lightvec.xz = rotate13(lightvec.xz, phase * 0.5 * 3.1415926);
+    uv.x += phase;
+  }
+
+  float lit = mix(1.0, max(0.0, dot(n, normalize(lightvec))), 0.98);
+  
+  lit += 0.3*pow(1.0 - max(0.0, n.z), 2.0);
+  
+  lit = sqrt(lit);
+ 
+  
+  uv *= 2.0;
+  vec3 color = image.eval((uv * 0.5 + 0.5) * iImageResolution).rgb;
+  
+return vec4(color,1.0);
 }`)!;
 
 export const Globe = () => {
@@ -168,9 +98,9 @@ export const Globe = () => {
   const earth = useImage(require("./assets/earth.jpg"));
   const uniforms = useComputedValue(() => {
     return {
-      iTime: clock.current / 600,
+      iTime: clock.current / 6000,
       iResolution: [width, height],
-      iImageResolution: [width, height], //[earth?.width() ?? 0, earth?.height() ?? 0],
+      iImageResolution: [earth?.width() ?? 0, earth?.height() ?? 0],
       r: center.x - 32,
       iMouse: touch.current,
     };
@@ -184,16 +114,7 @@ export const Globe = () => {
       {/* <Circle c={center} r={center.x - 32}> */}
       <Fill>
         <Shader source={source} uniforms={uniforms}>
-          <ImageShader
-            image={earth}
-            fit="fill"
-            x={0}
-            y={0}
-            tx="repeat"
-            ty="repeat"
-            width={100}
-            height={100}
-          />
+          <ImageShader image={earth} tx="repeat" ty="repeat" />
         </Shader>
       </Fill>
     </Canvas>
