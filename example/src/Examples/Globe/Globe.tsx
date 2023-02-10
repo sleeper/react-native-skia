32baaafd7816c06b407a1fac6efed5107db4b760
William Candillon <wcandillon@gmail.com>
2023-02-10 19:39:08 +0100

    :wrench:

--- example/src/Examples/Globe/Globe.tsx
+++ example/src/Examples/Globe/Globe.tsx
@@ -1,7 +1,8 @@
+import React from "react";
+import { View } from "react-native";
 import {
   Canvas,
-  Text,
-  Image,
+  Fill,
   ImageShader,
   Shader,
   Skia,
@@ -9,28 +10,19 @@ import {
   useComputedValue,
   useImage,
   useTouchHandler,
-  vec,
   useValue,
   Circle,
   BlurMask,
-  useFont,
-  Fill,
+  runDecay,
 } from "@shopify/react-native-skia";
-import React from "react";
-import { useWindowDimensions } from "react-native";
 
-// https://www.shadertoy.com/view/Ml2XRV
-// https://www.shadertoy.com/view/sdj3Rc
 const source = Skia.RuntimeEffect.Make(`
 uniform shader image;
-uniform shader clouds;
-
 uniform float iTime;
 uniform float2 iResolution;
 uniform float2 iImageResolution;
 uniform float2 iMouse;
-
-
+uniform float2 arc;
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
 const float MAX_DIST = 100.0;
@@ -42,25 +34,23 @@ mat2 rotate(float theta) {
   return mat2(c, -s, s, c);
 }
 
-float sdSphere(vec3 p, float r )
-{
+float sdSphere(vec3 p, float r) {
   return length(p) - r;
 }
 
 float sdScene(vec3 p) {
-  return sdSphere(p, 1.0);
+  float entity = sdSphere(p, 1.0);
+  return entity;
 }
 
 float rayMarch(vec3 ro, vec3 rd) {
   float depth = MIN_DIST;
-
   for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
     vec3 p = ro + depth * rd;
     float d = sdScene(p);
     depth += d;
     if (d < PRECISION || depth > MAX_DIST) break;
   }
-
   return depth;
 }
 
@@ -80,35 +70,23 @@ mat3 camera(vec3 cameraPos, vec3 lookAtPoint) {
 	vec3 cu = normalize(cross(cd, cr)); // camera up
 	return mat3(-cr, cu, -cd);
 }
-
-half4 main( vec2 fragCoord )
-{
+half4 main(vec2 fragCoord) {
   vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
   vec2 m = iMouse.xy/iResolution.xy;
-
   half3 col = vec3(0, 0, 0);
   
   vec3 lp = vec3(0); // lookat point (aka camera target)
   vec3 ro = vec3(0, 0, 3); // ray origin that represents camera position
-
   ro.yz *= rotate(mix(-PI, PI, m.y));
   ro.xz *= rotate(mix(-PI, PI, m.x));
-
   vec3 rd = camera(ro, lp) * normalize(vec3(uv, -1)); // ray direction
-
   float d = rayMarch(ro, rd);
-
   vec3 p = ro + rd * d;
   
   vec2 polarUV = vec2(atan(p.x, p.z)/PI, p.y/2.) + 0.5;
   polarUV.x -= iTime * 0.1;
   half3 bufferB = image.eval(polarUV * iImageResolution).rgb;
   half3 sphereColor = bufferB;
-
-  polarUV.x -= iTime * 0.05;
-  half3 bufferC = clouds.eval(polarUV * iImageResolution).rgb;
-  sphereColor = mix(sphereColor, bufferC, 0.3);
-  
   col = mix(col, sphereColor, step(d - MAX_DIST, 0.));
   if (col == vec3(0, 0, 0)) {
     return vec4(0, 0, 0, 0);
@@ -116,11 +94,25 @@ half4 main( vec2 fragCoord )
   return vec4(col, 1.0);
 }`)!;
 
-export const Globe = () => {
-  const font = useFont(require("../../assets/SF-Pro-Display-Bold.otf"), 46);
-  const clock = useClockValue();
-  const { width, height } = useWindowDimensions();
-  const center = vec(width / 2, height / 2);
+const arcColorStart = "#7629ed";
+const arcColorEnd = "#CB6BED";
+const arcsData = [
+  {
+    start: {
+      lat: 29.85587,
+      long: -95.4136,
+    },
+    end: {
+      lat: 34.05648,
+      long: -115.29487,
+    },
+  },
+];
+
+interface SkiaGlobeProps {}
+
+export const Globe = ({}: SkiaGlobeProps) => {
+  const size = 375;
   const x = useValue(0);
   const y = useValue(0);
   const offsetX = useValue(0);
@@ -134,54 +126,49 @@ export const Globe = () => {
       x.current = offsetX.current + pos.x;
       y.current = offsetY.current + pos.y;
     },
+    onEnd: ({ velocityX, velocityY }) => {
+      runDecay(x, { velocity: velocityX });
+      runDecay(y, { velocity: velocityY });
+    },
   });
-  const bg = useImage(
-    require("./assets/SPACE-BG-smaller-extend-down-tiny.png")
-  );
+
   const earth = useImage(require("./assets/earth.jpg"));
-  const clouds = useImage(require("./assets/clouds/2.jpg"));
+  const clock = useClockValue();
   const uniforms = useComputedValue(() => {
     return {
+      arc: [arcsData[0].start.lat, arcsData[0].start.long],
       iTime: clock.current / 2000,
-      iResolution: [width, height],
+      iResolution: [size, size],
       iImageResolution: [earth?.width() ?? 0, earth?.height() ?? 0],
       iMouse: [x.current, -y.current],
     };
   }, [clock, earth]);
-  if (!bg || !earth || !font || !clouds) {
+
+  if (!earth) {
     return null;
   }
   return (
-    <Canvas style={{ flex: 1 }} onTouch={onTouch}>
-      <Image image={bg} x={0} y={0} width={width} height={height} fit="cover" />
-      <Circle c={center} r={center.x + 100} color="#717b96">
-        <BlurMask blur={50} style="solid" />
-      </Circle>
-      <Fill>
-        <Shader
-          source={source}
-          uniforms={uniforms}
-          origin={center}
-          transform={[{ scale: 1 }]}
-        >
-          <ImageShader image={earth} tx="repeat" ty="repeat" />
-          <ImageShader image={clouds} tx="repeat" ty="repeat" />
-        </Shader>
-      </Fill>
-      {/* <Text
-        text="The Planet"
-        x={(width - font.getTextWidth("The Planet")) / 2}
-        y={center.y + 220}
-        font={font}
-        color="white"
-      />
-      <Text
-        text="Thanks You"
-        x={(width - font.getTextWidth("Thanks You")) / 2}
-        y={center.y + 300}
-        font={font}
-        color="white"
-      /> */}
-    </Canvas>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Canvas
+        style={{
+          width: size,
+          height: size,
+        }}
+        onTouch={onTouch}
+      >
+        <Fill>
+          <Shader source={source} uniforms={uniforms}>
+            <ImageShader image={earth} tx="repeat" ty="repeat" />
+          </Shader>
+        </Fill>
+      </Canvas>
+    </View>
   );
 };
